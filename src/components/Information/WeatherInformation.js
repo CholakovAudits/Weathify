f25bd5c98d1286353f351f44f49b5a8415be588e
Simeon Cholakov <simeonstefanovcholakov@gmail.com>
2023-08-18 14:43:00 +0300

    Added real feel, humidity and wind information

--- src/components/Information/WeatherInformation.js
+++ src/components/Information/WeatherInformation.js
@@ -1,4 +1,5 @@
-import Sun from './img/sun.png'
+import Sun from './img/sun.png';
+import { UilTemperature, UilWind, UilTear } from '@iconscout/react-unicons'
 
 const WeatherInformation = () => {
     return (
@@ -14,7 +15,21 @@ const WeatherInformation = () => {
                 />
                 <p className="text-4xl">35°</p>
                 <div className="flex flex-col space-y-2">
-
+                    <div className="flex font-light text-sm items-center justify-center">
+                        <UilTemperature size={20} className="mr-1" />
+                        <p className="text-base">Real feel:</p>
+                        <span className="text-base font-semibold ml-1">33°</span>
+                    </div>
+                    <div className="flex font-light text-sm items-center justify-center">
+                        <UilTear size={20} className="mr-1" />
+                        <p className="text-base">Humidity:</p>
+                        <span className="text-base font-semibold ml-1">34%</span>
+                    </div>
+                    <div className="flex font-light text-sm items-center justify-center">
+                        <UilWind size={20} className="mr-1" />
+                        <p className="text-base">Wind:</p>
+                        <span className="text-base font-semibold ml-1">3 km/h</span>
+                    </div>
                 </div>
             </div>
         </div>
